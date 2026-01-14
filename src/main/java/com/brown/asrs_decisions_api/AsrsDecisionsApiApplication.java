b1dcac8dc5f678f551b5ecf08ade819074eb8e07
Leeroy Brown <brownleeroy010@gmail.com>
2026-01-14 18:52:38 +0200

    initial commit

---
+++ src/main/java/com/brown/asrs_decisions_api/AsrsDecisionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_decisions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrsDecisionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsrsDecisionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brown/asrs_decisions_api/AsrsDecisionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_decisions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrsDecisionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
